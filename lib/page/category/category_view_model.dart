2632dde54687436682feebaae367adf6af79771e
MANH-HAUI <MANH-HAUI>
2022-11-27 23:44:31 +0700

    init category view model

--- lib/page/category/category_view_model.dart
+++ lib/page/category/category_view_model.dart
@@ -3,5 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'category_state.dart';
 
 class CategoryViewModel extends StateNotifier<CategoryState> {
-  CategoryViewModel(super.state);
+  CategoryViewModel(): super(CategoryState.initial());
 }
+
